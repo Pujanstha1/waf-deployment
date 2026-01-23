1a8fe0750b629b36a90085e156d1f886b353b3c7
Riwaj Chipalu <69961549+Reewaz-C@users.noreply.github.com>
2026-01-23 20:13:33 +0545

    Update main.tf: Removed SSH ingress

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -7,13 +7,13 @@ resource "aws_security_group" "firewall" {
   description = "Security group for EC2 instance"
   vpc_id      = var.vpc_id
 
-  ingress {
-    description = "SSH access"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.ssh_allowed_cidr]
-  }
+#ingress {
+#description = "SSH access"
+#from_port   = 22
+# to_port     = 22
+#  protocol    = "tcp"
+#   cidr_blocks = [var.ssh_allowed_cidr]
+#  }
 
   ingress {
     description = "Https"
