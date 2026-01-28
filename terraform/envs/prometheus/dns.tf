f43353849a4b81d8eb723c5bc44ee8b4f647d2ca
Riwaj Chipalu <69961549+Reewaz-C@users.noreply.github.com>
2026-01-28 08:41:45 +0545

    Update dns.tf

--- terraform/envs/prometheus/dns.tf
+++ terraform/envs/prometheus/dns.tf
@@ -2,10 +2,11 @@ resource "aws_route53_record" "prometheus" {
   zone_id = data.aws_route53_zone.dns.zone_id
   name    = "prometheus.tekbay.click"   
   type    = "A"
-  ttl     = 300
+  ttl     = 60
   records = [module.ec2.public_ip]
 
   lifecycle {
     prevent_destroy = true
   }
 }
+
